b83905e8a67b766d5007a6f694fbbeac8b164257
Marcel Rehnert <marcel817@szut.uni-bremen.de>
2018-11-21 12:56:56 +0100

    HelloWorld

--- RP2/src/GUI/Test.java
+++ RP2/src/GUI/Test.java
@@ -32,6 +32,7 @@ public class Test extends JFrame {
 	 */
 	public Test() {
 		int j = 0;
+		System.out.println("Hello World");
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
