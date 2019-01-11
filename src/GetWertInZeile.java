bec2d9a617b17fcf2b1eaa68adfdd2c9178df8c8
Marehno <blablacar325@web.de>
2019-01-11 12:51:15 +0100

    test

--- src/GetWertInZeile.java
+++ src/GetWertInZeile.java
@@ -1,73 +1,21 @@
-<<<<<<< HEAD
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 public class GetWertInZeile {
-
-	public GetWertInZeile(JTable kundenlisteTable) {
-		this.kundeInTabelleAuswaehlen(kundenlisteTable);
+	
+	public String getWertInZeile(JTable table) {
+		int zeile = table.getSelectedRow();
+		String rueckgabewert = (String) table.getValueAt(zeile, 0);
+		System.out.println(rueckgabewert);
+		return rueckgabewert;
 	}
-
-	public void kundeInTabelleAuswaehlen(JTable kundenlisteTable) {
-		kundenlisteTable.setCellSelectionEnabled(true);
-		ListSelectionModel cellSelectionModel = kundenlisteTable.getSelectionModel();
-		cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
-		cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
-			public void valueChanged(ListSelectionEvent e) {
-				String ausgewaehlterKunde = null;
-
-				int[] zeile = kundenlisteTable.getSelectedRows();
-				int[] spalte = kundenlisteTable.getSelectedColumns();
-
-				for (int i = 0; i < zeile.length; i++) {
-					for (int j = 0; j < spalte.length; j++) {
-						ausgewaehlterKunde = (String) kundenlisteTable.getValueAt(zeile[i], spalte[j]);
-						System.out.println(ausgewaehlterKunde);
-					}
-				}
-				System.out.println(ausgewaehlterKunde);
-			}
-		});
+	
+	public String getWertInZeileAusleihfenster(JTable table) {
+		int zeile = table.getSelectedRow();
+		String rueckgabewert = (String) table.getValueAt(zeile, 1);
+		System.out.println(rueckgabewert);
+		return rueckgabewert;
 	}
-
 }
-=======
-import javax.swing.JTable;
-import javax.swing.ListSelectionModel;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-
-public class KundeInTabelleAuswaehlen {
-
-	public KundeInTabelleAuswaehlen(JTable kundenlisteTable) {
-		// TODO Auto-generated constructor stub
-	}
-
-	public void kundeInTabelleAuswaehlen(JTable kundenlisteTable) {
-		kundenlisteTable.setCellSelectionEnabled(true);
-		ListSelectionModel cellSelectionModel = kundenlisteTable.getSelectionModel();
-		cellSelectionModel.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
-		cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
-			public void valueChanged(ListSelectionEvent e) {
-				String ausgewaehlterKunde = null;
-
-				int[] zeile = kundenlisteTable.getSelectedRows();
-				int[] spalte = kundenlisteTable.getSelectedColumns();
-
-				for (int i = 0; i < zeile.length; i++) {
-					for (int j = 0; j < spalte.length; j++) {
-						ausgewaehlterKunde = (String) kundenlisteTable.getValueAt(zeile[i], spalte[j]);
-						System.out.println(ausgewaehlterKunde);
-					}
-				}
-				System.out.println(ausgewaehlterKunde);
-			}
-		});
-	}
-
-}
->>>>>>> 210b9c96296de825c737a89edf86a3c4b0a4d511
--- src/KundenDAO.java
+++ src/KundenDAO.java
@@ -1,72 +1,19 @@
-<<<<<<< HEAD:src/KundenDAO.java
-import net.proteanit.sql.DbUtils;
-=======
-<<<<<<< HEAD
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-
-//import net.proteanit.sql.DbUtils;
->>>>>>> master:src/KundenverwaltungDAO.java
-
 import java.sql.*;
 
 public class KundenDAO {
-
-	// Methode zum Anzeigen aller Datensätze der Spalte name
-	public static ResultSet selectKunde(String kunde) throws ClassNotFoundException {
+	
+	ConnectToDB connect = new ConnectToDB();
+	
+	// Methode zum Anzeigen aller Datensï¿½tze der Spalte name
+	public ResultSet selectKunde(String kunde) throws ClassNotFoundException {
+		Connection conn = connect.connectToDB();
 		try {
-			String sql = "SELECT DISTINCT * FROM test WHERE LOWER(vorname) = '" + kunde.toLowerCase() + "'";
-			// connect()-Methode wird ausgeführt um eine Verbindung zur Datenbank
+			String sql = "SELECT DISTINCT Kunden.id, Kunden.vorname, Kunden.nachname, Kunden.strasse FROM Kunden WHERE LOWER(Kunden.vorname) = '" + kunde.toLowerCase() + "'";
+			// connect()-Methode wird ausgefï¿½hrt um eine Verbindung zur Datenbank
 			// herzustellen
-			Connection conn = ConnectToDB.connectToDB();
-			Statement statement = conn.createStatement();
-			ResultSet rs = statement.executeQuery(sql);
-			// Gibt Nachricht aus bei funktionierendem SELECT
-			System.out.println("SQL-SELECT funzt");
-			return rs;
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
-			return null;
-		}
-	}
-	public void Zurueckgeben (Spiele spiele) throws ClassNotFoundException {
-		Connection conn = ConnectToDB.connectToDB();
-		Spiele spiel = new Spiele();
-
-		try {
-			String titel = Spiele.getTitel();
-			String kdnnachname = getKdnnachname();
-			Statement statement;
-			statement = conn.createStatement();
-			statement.executeUpdate("SELECT * FROM Kunde-Spiele where Spiel =" );
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();  
-		}finally  {
-			
-		}
-		
-	}
-}
-=======
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-
-//import net.proteanit.sql.DbUtils;
-
-import java.sql.*;
-
-public class KundenverwaltungDAO {
-
-	// Methode zum Anzeigen aller Datensätze der Spalte name
-	public static ResultSet selectKunde(String kunde) throws ClassNotFoundException {
-		try {
-			String sql = "SELECT DISTINCT * FROM test WHERE LOWER(vorname) = '" + kunde.toLowerCase() + "'";
-			// connect()-Methode wird ausgeführt um eine Verbindung zur Datenbank
-			// herzustellen
-			Connection conn = ConnectToDB.connectToDB();
 			Statement statement = conn.createStatement();
 			ResultSet rs = statement.executeQuery(sql);
 			// Gibt Nachricht aus bei funktionierendem SELECT
@@ -78,4 +25,3 @@ public class KundenverwaltungDAO {
 		}
 	}
 }
->>>>>>> 210b9c96296de825c737a89edf86a3c4b0a4d511
